53f5a54eef5e69d1c1a0c9e8d396028f6435742a
Raghav2709 <152988951+Raghav2709@users.noreply.github.com>
2024-01-24 15:43:13 +0530

    create posts page ready

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js'
+import postRoutes from './routes/post.route.js'
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 
@@ -12,6 +14,7 @@ mongoose.connect(process.env.MONGO).then(
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, ()=>{
     console.log("server is running on port 3000");
@@ -19,6 +22,7 @@ app.listen(3000, ()=>{
 
 app.use('/server/user',userRoutes);
 app.use('/server/auth',authRoutes);
+app.use('/server/post', postRoutes);
 
 app.use((err,req,res,next) => {
     const statusCode = err.statusCode || 500;
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import { deleteUser, signout, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test',(req,res) => {
     res.json({message:'Hello World'});
 });
+router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
+router.post('/signout', signout);
 
 
 export default router;
